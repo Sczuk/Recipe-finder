ac52530a5de06a1ba616425b291b26785268e2b0
Sczuk <hgreasczuk@gmail.com>
2026-04-20 16:42:21 -0300

    style(migration): removed the semicolon

--- src/main/resources/db/migration/V0.1__creating_table_user.sql
+++ src/main/resources/db/migration/V0.1__creating_table_user.sql
@@ -3,4 +3,4 @@ Create table User
     id       int          primary key unique not null,
     login    varchar(100)             unique not null,
     password varchar(100)                    not null
-);
+)
