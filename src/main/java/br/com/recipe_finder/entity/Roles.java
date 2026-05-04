9e4ec9ba94b1991120b2d4bae8ba4147b243aaea
Sczuk <hgreasczuk@gmail.com>
2026-05-04 16:05:58 -0300

    feature(permissions): add the Permissions of roles

--- src/main/java/br/com/recipe_finder/entity/Roles.java
+++ src/main/java/br/com/recipe_finder/entity/Roles.java
@@ -1,8 +1,61 @@
 package br.com.recipe_finder.entity;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.List;
+import java.util.Set;
+
+@Getter
+@RequiredArgsConstructor
 public enum Roles {
-    ADMIN,
-    CHEF,
-    BARMAN,
-    USER
+    ADMIN(
+            Set.of(
+                    Permissions.ADMIN_GET,
+                    Permissions.ADMIN_DELETE,
+                    Permissions.ADMIN_POST,
+                    Permissions.BARMAN_GET,
+                    Permissions.BARMAN_POST,
+                    Permissions.CHEF_GET,
+                    Permissions.CHEF_POST,
+                    Permissions.USER_POST,
+                    Permissions.USER_DELETE,
+                    Permissions.USER_GET,
+                    Permissions.USER_PUT
+            )
+    ),
+    CHEF(
+            Set.of(
+                    Permissions.CHEF_GET,
+                    Permissions.CHEF_POST
+            )
+    ),
+    BARMAN(
+            Set.of(
+                    Permissions.BARMAN_GET,
+                    Permissions.BARMAN_POST
+            )
+    ),
+    USER(
+            Set.of(
+                    Permissions.USER_POST,
+                    Permissions.USER_DELETE,
+                    Permissions.USER_GET,
+                    Permissions.USER_PUT
+            )
+    );
+
+
+    private final Set<Permissions> permissions;
+
+    public List<SimpleGrantedAuthority> getAuthorities(){
+        var authorities = new java.util.ArrayList<>(getPermissions()
+                .stream()
+                .map(permission -> new SimpleGrantedAuthority(permission.getPermission()))
+                .toList());
+        authorities.add(new SimpleGrantedAuthority("ROLE_" + this.name()));
+        return authorities;
+    }
+
 }
